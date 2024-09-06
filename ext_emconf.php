de03f69395b2a6c8719aa50cb654e3ec415e788b
Thomas Rawiel <thomasrawiel@users.noreply.github.com>
2024-09-06 10:31:00 +0200

    initial commit

---
+++ ext_emconf.php
@@ -0,0 +1,19 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'VCF-QR',
+    'description' => 'Create QR-Code with VCF card',
+    'category' => 'misc',
+    'author' => 'Thomas Rawiel',
+    'author_email' => 'thomas.rawiel@gmail.com',
+    'state' => 'alpha',
+    'clearCacheOnLoad' => 0,
+    'version' => '0.1.0',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '12.4.0-13.9.99',
+        ],
+        'conflicts' => [],
+        'suggests' => [],
+    ],
+];
