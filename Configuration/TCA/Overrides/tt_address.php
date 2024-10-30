eafb4bdc8478a339effca112a6629d77cfc8d805
Thomas Rawiel <thomasrawiel@users.noreply.github.com>
2024-10-30 11:00:43 +0100

    [bugfix] fix typo in locallang path

--- Configuration/TCA/Overrides/tt_address.php
+++ Configuration/TCA/Overrides/tt_address.php
@@ -6,13 +6,14 @@ call_user_func(function ($_EXTKEY = 'vcfqr', $table = 'tt_address') {
     if (\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('tt_address')) {
         \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns($table, [
             'hideqrcode' => [
-                'label' => 'LLL:EXt:vcfqr/Resources/Private/Language/locallang_be.xlf:tt_address.hideqrcode',
+                'label' => 'LLL:EXT:vcfqr/Resources/Private/Language/locallang_be.xlf:tt_address.hideqrcode',
                 'config' => [
                     'type' => 'check',
                     'renderType' => 'checkboxToggle',
                     'items' => [
                         ['label' => '', 'invertStateDisplay' => true,],
                     ],
+                    'default' => 1,
                 ],
             ],
         ]);
